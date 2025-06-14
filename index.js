71cf94c10bce4cd3e80e4a5dbe465386b522219e
Namalabharath <164044328+Namalabharath@users.noreply.github.com>
2025-06-15 00:09:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://book-store-app-frontend-yp5v.vercel.app'],
+  origin: ['http://localhost:5173', 'book-store-app-frontend-six.vercel.app'],
   credentials: true
 }))
 

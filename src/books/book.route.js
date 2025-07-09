25a325163555f663b2f77a9025d8152ed2aa3bb0
Bharath Kumar <164044328+Namalabharath@users.noreply.github.com>
2025-07-09 19:06:18 +0530

    Update book.route.js

--- src/books/book.route.js
+++ src/books/book.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Book = require('./book.model');
 const { postABook, getAllBooks, getSingleBook, UpdateBook, deleteABook } = require('./book.controller');
-//const verifyAdminToken = require('../middleware/verifyAdminToken');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
 const router =  express.Router();
 
 // frontend => backend server => controller => book schema  => database => send to server => back to the frontend
@@ -11,7 +11,7 @@ const router =  express.Router();
 // delete = when delete something
 
 // post a book
-router.post("/create-book",postABook)
+router.post("/create-book", verifyAdminToken, postABook)
 
 // get all books
 router.get("/", getAllBooks);
@@ -20,9 +20,9 @@ router.get("/", getAllBooks);
 router.get("/:id", getSingleBook);
 
 // update a book endpoint
-router.put("/edit/:id", UpdateBook);
+router.put("/edit/:id", verifyAdminToken, UpdateBook);
 
-router.delete("/:id",  deleteABook)
+router.delete("/:id", verifyAdminToken, deleteABook)
 
 
 module.exports = router;
